70c6f354d79052cba272ad519896441748837e43
ridam258 <ridam258@gmail.com>
2021-02-24 00:37:20 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,67 @@
+const app = Vue.createApp({
+    data(){
+        return{
+            budgetLogs: [],
+            totalBudget: 0,
+            totalIncome: 0,
+            totalExpenses: 0,
+            dropdownValue: "inc",
+            inputValue:'',
+            descriptionValue:'',
+        };
+    },
+    computed:{
+        buttonLabel(){
+            if(this.dropdownValue==="inc"){
+                return "Income";
+            }
+            else if(this.dropdownValue==='exp'){
+                return "Expend";
+            }
+            else{
+                return "Income";
+            }
+        },
+    },
+    methods: {
+        callMethod(){
+            this.calculateTotalBudget();
+            this.calculateTotalIncome();
+            if(this.inputValue){
+                this.addBudgetLog(this.dropdownValue, this.inputValue, this.descriptionValue);
+            }
+        },
+        calculateTotalBudget(){
+            if(this.dropdownValue==="inc"){
+                this.totalBudget+=parseFloat(this.inputValue);
+            }
+            else if(this.dropdownValue==="exp"){
+                this.totalBudget-=parseFloat(this.inputValue);
+            }
+        },
+        calculateTotalIncome(){
+            console.log('hi');
+            
+            if(this.dropdownValue==="inc"){
+                this.totalIncome+=parseFloat(this.inputValue);
+            }
+            else if(this.dropdownValue==="exp"){
+                this.totalExpenses+=parseFloat(this.inputValue);
+            }
+        },
+        getValue(e){
+            this.dropdownValue=e.target.value;
+            console.log(this.dropdownValue);
+        },
+        addBudgetLog(action, value, description ){
+            this.budgetLogs.unshift({
+                actionType:action,
+                actionvalue: value,
+                descriptionvalue: description,
+
+            })
+        }
+    }
+})
+
+app.mount("#app");
